9d36fa12ae8468f48cef1bab524bfaf196b7d9d8
Dmitri Tkach <dmitri.tkach@gcore.com>
2024-07-23 13:34:55 +0400

    Remove irrelevant demos Amend README

--- apps/gvcdemo/src/components/peers/Logger.ts
+++
@@ -1,3 +0,0 @@
-import { Logger } from "@gcorevideo/rtckit"
-
-export const logger = new Logger("components.Peers")
--- apps/gvcdemo/src/components/rtcUtils.ts
+++
@@ -1,7 +0,0 @@
-import { RtcClient, type SignalConnection } from "@gcorevideo/rtckit"
-
-export function useRtcClient(conn: SignalConnection) {
-  const rtcClient = new RtcClient(conn)
-
-  return rtcClient
-}
