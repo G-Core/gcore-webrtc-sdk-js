74a4ddda5a040ddf48f3e6d2d9aec18fbbed2a64
Dmitri Tkach <dmitri.tkach@gcore.com>
2024-11-18 13:08:03 +0400

    Update rtckit in demo app, expose version in console logs

--- apps/webrtc-demo/src/main.ts
+++ apps/webrtc-demo/src/main.ts
@@ -1,16 +1,18 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import pkg from "../package.json" with { type: "json" };
+import pkg from "../package-lock.json" with { type: "json" };
 import App from './App.vue'
 import router from './router'
 
 import { LogTracer, Logger, setTracer } from '@gcorevideo/rtckit';
 
+const rtckitVersion = pkg.packages["node_modules/@gcorevideo/rtckit"].version;
+
 console.log(
     `${pkg.name} ${
       import.meta.env.VITE_APP_VERSION
-    }`,
+    }/rtckit ${rtckitVersion}`,
   )
 
 Logger.enable(localStorage.getItem("debug") || "*:ERROR:*,*:WARN:*");
